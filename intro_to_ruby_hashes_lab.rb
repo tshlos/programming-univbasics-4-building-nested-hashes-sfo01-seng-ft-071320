faff1e63c70bc5ba5af129f5bb4ce70a533a594c
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-14 21:47:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -55,28 +55,4 @@ def monopoly_with_fourth_tier
     }
   }
   
-     hash = { 
-       :railroads =>
-         :names=>
-        {
-          :reading_railroad=>
-          {
-            "mortgage_value"=>"$100"
-          },
-          :pennsylvania_railroad=>
-          {
-            "mortgage_value"=>"$200"
-          },
-          :b_and_o_railroad=>
-          {
-            "mortgage_value"=>"$400"
-          },
-          :shortline=>
-          {
-            "mortgage_value"=>"$800"
-          }
-        }
-     }  
-     }
-  
 end
